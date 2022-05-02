974050b470aead826d06b2f5479a7e000c8c7e2a
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-02 18:26:10 +0700

    better adapter typing

--- src/core/UseCaseAdapter.ts
+++ src/core/UseCaseAdapter.ts
@@ -1,16 +1,15 @@
 import Joi from "joi";
 
-type AdapterConstructor = { name: string; SCHEMA?: Joi.ObjectSchema };
-
 export abstract class UseCaseAdapter<Params = unknown> {
 	private params!: Partial<Params>;
+	static SCHEMA?: Joi.ObjectSchema;
 
 	constructor(params: Partial<Params>) {
 		this.params = params;
 	}
 
 	public async validate(): Promise<Params> {
-		const Adapter = this.constructor as unknown as AdapterConstructor;
+		const Adapter = this.constructor as typeof UseCaseAdapter;
 		const result = await Adapter.SCHEMA?.validateAsync(this.params);
 		if (!result) throw new Error(`Couldn't validate ${Adapter.name}`);
 		return result;

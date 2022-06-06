eca13296d55781f3e1725087bece6032bd3e52ec
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-06 10:14:39 +0700

    underline now playing track

--- src/apps/discord/commands/QueueCommand.ts
+++ src/apps/discord/commands/QueueCommand.ts
@@ -1,4 +1,8 @@
 import {
+	GetNowPlayingAdapter,
+	GetNowPlayingUseCase,
+} from "@modules/queue/useCases/GetNowPlayingUseCase";
+import {
 	GetQueueTracksAdapter,
 	GetQueueTracksUseCase,
 } from "@modules/queue/useCases/GetQueueTracksUseCase";
@@ -12,29 +16,44 @@ export class QueueCommand implements ICommand {
 	public readonly aliases = ["q"];
 	public readonly description = "Show current queue";
 
-	constructor(@inject(GetQueueTracksUseCase) private getQueueTracks: GetQueueTracksUseCase) {}
+	constructor(
+		@inject(GetQueueTracksUseCase) private getQueueTracks: GetQueueTracksUseCase,
+		@inject(GetNowPlayingUseCase) private getNowPlaying: GetNowPlayingUseCase
+	) {}
 
 	public async execute({ message, args }: CommandExecuteProps): Promise<void> {
 		const page = Number(args.shift() || 1);
 		const perPage = 10;
 
-		const adapter = new GetQueueTracksAdapter({
+		const queueTracksAdapter = new GetQueueTracksAdapter({
 			guildId: message.guild?.id,
 			page,
 			perPage,
 		});
-		const { tracks, totalLength } = await this.getQueueTracks.execute(adapter, {
-			userId: message.author.id,
+
+		const nowPlayingAdapter = new GetNowPlayingAdapter({
+			guildId: message.guild?.id,
 		});
+
+		const [{ tracks, totalLength }, nowPlaying] = await Promise.all([
+			this.getQueueTracks.execute(queueTracksAdapter, { userId: message.author.id }),
+			this.getNowPlaying.execute(nowPlayingAdapter, { userId: message.author.id }),
+		]);
 
 		const start = (page - 1) * perPage;
 		const embed = new MessageEmbed({
 			title: "Queue",
 			description: `Showing page **${page}** / **${Math.ceil(totalLength / perPage)}**`,
-			fields: tracks.map((track, index) => ({
-				name: `${start + index + 1}. ${track.video.title}`,
-				value: `${track.url}\r\nRequested by <@!${track.requestedBy?.id}>`,
-			})),
+			fields: tracks.map((track, index) => {
+				let name = `${start + index + 1}. ${track.video.title}`;
+				console.log(track.id);
+				if (track.id === nowPlaying?.id) name = `__${name}__`;
+
+				return {
+					name,
+					value: `${track.url}\r\nRequested by <@!${track.requestedBy?.id}>`,
+				};
+			}),
 		});
 
 		await message.reply({ embeds: [embed] });

53189e06f3165dff9c7b8304cb3a60fa096c0da1
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-07 16:25:37 +0700

    remove event emitter

--- src/modules/user/entities/UserPlayHistory.ts
+++ src/modules/user/entities/UserPlayHistory.ts
@@ -1,19 +1,15 @@
-import { EventEmitter } from "events";
-
 interface ConstructorProps {
 	videoId: string;
 	userId: string;
 	playedAt: Date;
 }
 
-export class UserPlayHistory extends EventEmitter {
+export class UserPlayHistory {
 	public readonly videoId: string;
 	public readonly userId: string;
 	public readonly playedAt: Date;
 
 	constructor(props: ConstructorProps) {
-		super();
-
 		this.videoId = props.videoId;
 		this.userId = props.userId;
 		this.playedAt = props.playedAt;

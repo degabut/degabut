4fbb5898b0f450f003425de41cd7cf83cf5c49f7
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-08-12 16:57:51 +0700

    split host and port

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -13,20 +13,24 @@ import { DatabaseConfigModule } from "./config";
     ObjectionModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        config: {
-          client: "pg",
-          connection: {
-            ...config.getOrThrow<any>("database"),
-            port: 5432,
+      useFactory: (config: ConfigService) => {
+        const [host, port] = config.get("database.host").split(":");
+        return {
+          config: {
+            client: "pg",
+            connection: {
+              ...config.getOrThrow<any>("database"),
+              host,
+              port: +(port || 5432),
+            },
+            migrations: {
+              directory: path.join(__dirname, "./migrations"),
+              tableName: "knex_migrations",
+              loadExtensions: [".js"],
+            },
           },
-          migrations: {
-            directory: path.join(__dirname, "./migrations"),
-            tableName: "knex_migrations",
-            loadExtensions: [".js"],
-          },
-        },
-      }),
+        };
+      },
     }),
     ObjectionModule.forFeature([UserPlayHistoryModel, VideoModel, ChannelModel]),
   ],

d81c903c28d0b8a94146fd14fb0c6427177dee28
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-09 19:50:14 +0700

    remove log

--- src/youtube/services/youtube.service.ts
+++ src/youtube/services/youtube.service.ts
@@ -22,8 +22,6 @@ export class YoutubeService {
 
       video = this.videoToVideoCompact(newVideo);
       await this.cacheVideo(video);
-    } else {
-      console.log("get cached");
     }
 
     return video;

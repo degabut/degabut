cd93fe19796674d1a377226783ac0a42063bab63
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-26 17:47:48 +0700

    set playedAt to null

--- src/queue/listeners/track-audio-ended.handler.ts
+++ src/queue/listeners/track-audio-ended.handler.ts
@@ -9,6 +9,7 @@ export class TrackAudioEndedHandler implements IEventHandler<TrackAudioEndedEven
   public async handle({ track }: TrackAudioEndedEvent): Promise<void> {
     const queue = track.queue;
     if (queue.shuffle) queue.shuffleHistoryIds.push(track.id);
+    track.playedAt = null;
     this.queueService.processQueue(track.queue);
   }
 }

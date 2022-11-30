6324cfcab7af25ea14bd562790ee5d406f5d4318
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-01 02:58:12 +0700

    fix env key

--- src/discord-bot/config/discord-bot-config.module.ts
+++ src/discord-bot/config/discord-bot-config.module.ts
@@ -6,7 +6,7 @@ export const discordBotConfig = registerAs("discord-bot", () => ({
   token: process.env.TOKEN as string,
   prefix: process.env.PREFIX as string,
   lavalinkHost: process.env.LAVA_HOST as string,
-  lavalinkPassword: process.env.LAVA_PASS as string,
+  lavalinkPassword: process.env.LAVA_PASSWORD as string,
   lavalinkPort: +(process.env.LAVA_PORT as string),
 }));
 
@@ -18,7 +18,7 @@ export const discordBotConfig = registerAs("discord-bot", () => ({
         TOKEN: Joi.string().required(),
         PREFIX: Joi.string().required(),
         LAVA_HOST: Joi.string().required(),
-        LAVA_PASS: Joi.string().required(),
+        LAVA_PASSWORD: Joi.string().required(),
         LAVA_PORT: Joi.string(),
       }),
       validationOptions: {

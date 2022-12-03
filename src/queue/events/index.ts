4a466bf43e88ddf084444516f39460e8b902d129
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-03 11:58:57 +0700

    rename

--- src/queue/events/index.ts
+++ src/queue/events/index.ts
@@ -5,7 +5,7 @@ export * from "./member-updated.event";
 export * from "./queue-cleared.event";
 export * from "./queue-created.event";
 export * from "./queue-destroyed.event";
-export * from "./queue-loop-type-changed.event";
+export * from "./queue-loop-mode-changed.event";
 export * from "./queue-processed.event";
 export * from "./queue-shuffle-toggled.event";
 export * from "./track-added.event";

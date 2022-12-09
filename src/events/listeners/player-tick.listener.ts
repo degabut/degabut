d0cedf10b4b14191098ca04543b60d72829acd5f
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-09 21:56:15 +0700

    dont send tick event if paused

--- src/events/listeners/player-tick.listener.ts
+++ src/events/listeners/player-tick.listener.ts
@@ -7,6 +7,8 @@ export class PlayerTickListener implements IEventHandler<PlayerTickEvent> {
   constructor(private readonly gateway: EventsGateway) {}
 
   public async handle({ player }: PlayerTickEvent): Promise<void> {
+    if (player.audioPlayer.paused) return;
+
     const memberIds = player.voiceChannel.members.map((m) => m.id);
     this.gateway.send(memberIds, "player-tick", { position: player.audioPlayer.position });
   }

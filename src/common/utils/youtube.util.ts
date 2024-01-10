faf0d668c0b8957adae1ad147b437c623401ee5b
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-01-10 09:31:53 +0700

    fix not extracting mobile url

--- src/common/utils/youtube.util.ts
+++ src/common/utils/youtube.util.ts
@@ -4,16 +4,19 @@ type YoutubeIds = {
 };
 
 export class YoutubeUtil {
-  static HOSTS = ["youtube.com", "youtu.be", "music.youtube.com"];
+  static HOSTS = ["youtube.com", "youtu.be"];
 
   static extractIds(urlString: string): YoutubeIds {
     try {
       const url = new URL(urlString);
-      const host = url.host.replace(/^www\./, "");
+      const [domain, tld] = url.hostname.split(".").slice(-2);
+      const host = `${domain}.${tld}`;
 
       if (!YoutubeUtil.HOSTS.includes(host)) return {};
 
-      const videoId = url.searchParams.get("v") || url.pathname.split("/").pop();
+      const videoId =
+        url.searchParams.get("v") ||
+        (host === "youtube.be" ? url.pathname.split("/").pop() : undefined);
       const playlistId = url.searchParams.get("list") || undefined;
 
       return { videoId, playlistId };

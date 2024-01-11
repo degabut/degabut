fd11810253d4288a5ccb16afffa99d5542015301
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-01-11 18:42:06 +0700

    increase max track age

--- src/youtube/youtube.constants.ts
+++ src/youtube/youtube.constants.ts
@@ -1,4 +1,4 @@
-export const MAX_VIDEO_AGE = 60 * 60 * 24 * 7; // 7 days
+export const MAX_VIDEO_AGE = 60 * 60 * 24 * 14; // 14 days
 export const MAX_PLAYLIST_VIDEOS_PAGE = 3; // up to 300
 
 export const YOUTUBEI_PROVIDER = "YOUTUBEI_PROVIDER";

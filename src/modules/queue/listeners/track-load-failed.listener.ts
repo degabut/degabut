b31db943bdfd59e907e79da412301650ff95e91d
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-01 16:19:10 +0700

    fix typing

--- src/modules/queue/listeners/track-load-failed.listener.ts
+++ src/modules/queue/listeners/track-load-failed.listener.ts
@@ -1,13 +1,16 @@
 import { EventBus, EventsHandler, IEventHandler } from "@nestjs/cqrs";
-import { TrackAudioEndedEvent, TrackLoadFailedEvent } from "@queue-player/events";
+import { TrackLoadFailedEvent } from "@queue-player/events";
 import { TrackRemovedEvent } from "@queue/events";
 import { QueueService } from "@queue/services";
 
 @EventsHandler(TrackLoadFailedEvent)
-export class TrackLoadFailedListener implements IEventHandler<TrackAudioEndedEvent> {
-  constructor(private readonly eventBus: EventBus, private readonly queueService: QueueService) {}
+export class TrackLoadFailedListener implements IEventHandler<TrackLoadFailedEvent> {
+  constructor(
+    private readonly eventBus: EventBus,
+    private readonly queueService: QueueService,
+  ) {}
 
-  public async handle({ track }: TrackAudioEndedEvent): Promise<void> {
+  public async handle({ track }: TrackLoadFailedEvent): Promise<void> {
     const queue = track.queue;
     queue.tracks = queue.tracks.filter((t) => t.id !== track.id);
     this.eventBus.publish(new TrackRemovedEvent({ isNowPlaying: true, track }));

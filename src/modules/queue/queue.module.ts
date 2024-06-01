599617a969766c1c6df3f9cea229768324867e17
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-01 13:42:13 +0700

    add missing dependency

--- src/modules/queue/queue.module.ts
+++ src/modules/queue/queue.module.ts
@@ -3,6 +3,7 @@ import { MediaSourceModule } from "@media-source/media-source.module";
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { PlaylistModule } from "@playlist/playlist.module";
+import { UserModule } from "@user/user.module";
 import { YoutubeModule } from "@youtube/youtube.module";
 
 import { Commands } from "./commands";
@@ -12,7 +13,14 @@ import { QueueRepository } from "./repositories";
 import { QueueService } from "./services";
 
 @Module({
-  imports: [CqrsModule, YoutubeModule, MediaSourceModule, PlaylistModule, HistoryModule],
+  imports: [
+    CqrsModule,
+    YoutubeModule,
+    MediaSourceModule,
+    PlaylistModule,
+    HistoryModule,
+    UserModule,
+  ],
   providers: [QueueRepository, QueueService, ...Commands, ...Queries, ...Listeners],
   exports: [QueueRepository],
 })
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,7 @@
 import { DatabaseModule } from "@database/database.module";
 import { HistoryModule } from "@history/history.module";
 import { MediaSourceModule } from "@media-source/media-source.module";
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { QueueModule } from "@queue/queue.module";
 
@@ -10,7 +10,13 @@ import { Queries } from "./queries";
 import { UserLikeMediaSourceRepository } from "./repositories";
 
 @Module({
-  imports: [CqrsModule, DatabaseModule, QueueModule, MediaSourceModule, HistoryModule],
+  imports: [
+    CqrsModule,
+    DatabaseModule,
+    forwardRef(() => QueueModule), // TODO remove dependency
+    MediaSourceModule,
+    HistoryModule,
+  ],
   providers: [UserLikeMediaSourceRepository, ...Queries, ...Commands],
   exports: [UserLikeMediaSourceRepository],
 })

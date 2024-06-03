3957bcb2182f71e49dc22a6709956efb36fde399
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-03 13:58:39 +0700

    remove unused prop

--- src/modules/queue/listeners/track-load-failed.listener.ts
+++ src/modules/queue/listeners/track-load-failed.listener.ts
@@ -1,10 +1,8 @@
-import { EventBus, EventsHandler, IEventHandler } from "@nestjs/cqrs";
+import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
 import { TrackLoadFailedEvent } from "@queue-player/events";
 
 @EventsHandler(TrackLoadFailedEvent)
 export class TrackLoadFailedListener implements IEventHandler<TrackLoadFailedEvent> {
-  constructor(private readonly eventBus: EventBus) {}
-
   public async handle({ track }: TrackLoadFailedEvent): Promise<void> {
     const queue = track.queue;
     queue.removeTrack({ trackId: track.id });

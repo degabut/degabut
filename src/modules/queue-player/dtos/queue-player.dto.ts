534667c9080df11eaf5a0fc5180246f3b27d7450
SuspiciousLookingOwl <vinjongm@gmail.com>
2025-08-05 09:39:20 +0800

    fix filters not returned

--- src/modules/queue-player/dtos/queue-player.dto.ts
+++ src/modules/queue-player/dtos/queue-player.dto.ts
@@ -1,4 +1,5 @@
 import { QueuePlayer } from "@queue-player/entities";
+import { PlayerFilters } from "@queue-player/providers";
 import { Exclude, Expose, plainToInstance, Transform } from "class-transformer";
 
 @Exclude()
@@ -14,6 +15,10 @@ export class QueuePlayerDto {
   @Expose()
   @Transform(({ obj }: { obj: QueuePlayer }) => obj.audioPlayer.isSeekable)
   public isSeekable!: boolean;
+
+  @Expose()
+  @Transform(({ obj }: { obj: QueuePlayer }) => obj.audioPlayer.filters)
+  public filters!: PlayerFilters;
 
   public static create(entity: QueuePlayer): QueuePlayerDto {
     return plainToInstance(QueuePlayerDto, entity);

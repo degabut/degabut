4084055b50985762bc71955868c98b3da01e894d
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-02-06 16:35:39 +0800

    allow null validation

--- src/common/decorators/validate-params.decorator.ts
+++ src/common/decorators/validate-params.decorator.ts
@@ -2,13 +2,19 @@ import { MethodDecorator } from "@common/interfaces";
 import { BadRequestException } from "@nestjs/common";
 import { Schema } from "joi";
 
-export const ValidateParams = (...schemas: Schema[]): MethodDecorator<(...args: any[]) => any> => {
+export const ValidateParams = (
+  ...schemas: (Schema | null)[]
+): MethodDecorator<(...args: any[]) => any> => {
   return (t, p, descriptor) => {
     const method = descriptor.value;
 
     descriptor.value = async function (this: any, ...args: unknown[]) {
       const validatedArgs: unknown[] = [];
       for (const [i, schema] of schemas.entries()) {
+        if (!schema) {
+          validatedArgs.push(args[i]);
+          continue;
+        }
         const result = schema.validate(args[i], { stripUnknown: true });
         if (result.error) throw new BadRequestException(result.error.message);
         validatedArgs.push(result.value);
